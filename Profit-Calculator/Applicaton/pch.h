7c023dc5496e7748e1314fcb7cde6d18892fdcf6
Scott <scott@nightsky.cc>
2018-12-06 16:08:05 -0700

    Nothing was working.. Started again

---
+++ Profit-Calculator/Applicaton/pch.h
@@ -0,0 +1,32 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <utility>
+
+// Data Structures
+
+#include <string>
+#include <stack>
+//#include <deque>
+#include <array>
+#include <vector>
+//#include <set>
+//#include <map>
+//#include <unordered_set>
+//#include <unordered_map>
+
+// File Manipulation
+
+#include <fstream>
+
+// Windows API
+
+#include <windows>
+
+
+#endif //PCH_H
--- Profit-Calculator/Profit-Calculator/main.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "pch.h"
-
-#include "Tools.h" // Tools class
-//#include "" // Main program class
